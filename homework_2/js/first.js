a4f919a5fb42aa0278ea3f413856b48934801faa
Demianenko Nataliia <demiia.art@gmail.com>
2023-09-26 09:24:31 +0200

    2

---
+++ homework_2/js/first.js
@@ -0,0 +1,15 @@
+// let str = prompt('Enter your name here', '');
+// alert('Hello, '+str+'! How are you?');
+
+const firstNumber = prompt('Enter first number', );
+const secondNumber = prompt('Enter second number', );
+
+
+alert(+firstNumber+'+'+secondNumber+'='+(+firstNumber + +secondNumber));
+
+alert(+firstNumber+'-'+secondNumber+'='+(firstNumber - secondNumber));
+
+alert(+firstNumber+'*'+secondNumber+'='+(firstNumber * secondNumber));
+
+alert(+firstNumber+'/'+secondNumber+'='+(firstNumber / secondNumber));
+
