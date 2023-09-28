3ed151b65c281a503b428b056d709af4073be259
Demianenko Nataliia <demiia.art@gmail.com>
2023-09-28 09:07:36 +0200

    fix

--- homework_2/js/mathematic.js
+++ homework_2/js/mathematic.js
@@ -1,15 +1,12 @@
-// let str = prompt('Enter your name here', '');
-// alert('Hello, '+str+'! How are you?');
 
-const firstNumber = prompt('Enter first number', );
-const secondNumber = prompt('Enter second number', );
+const firstNumber = +prompt('Enter first number', );
+const secondNumber = +prompt('Enter second number', );
 
+alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
 
-alert(+firstNumber+'+'+secondNumber+'='+(+firstNumber + +secondNumber));
+alert(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
 
-alert(+firstNumber+'-'+secondNumber+'='+(firstNumber - secondNumber));
+alert(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
 
-alert(+firstNumber+'*'+secondNumber+'='+(firstNumber * secondNumber));
-
-alert(+firstNumber+'/'+secondNumber+'='+(firstNumber / secondNumber));
+alert(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
 
--- homework_2/js/time.js
+++
@@ -1,9 +0,0 @@
-let whatTime = prompt('Hello! Write how much hours you want to convert:');
-
-const seconds = +whatTime * 60 * 60;
-
-if (whatTime <= 1) {
-    alert(+whatTime+' hour = '+seconds+' seconds');
-} else {
-    alert(+whatTime+' hours = '+seconds+' seconds');
-}
