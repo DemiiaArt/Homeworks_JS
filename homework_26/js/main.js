c9756ef5afb661c9019117f95e1e411d9d2f1a46
Demianenko Nataliia <demiia.art@gmail.com>
2023-11-17 11:54:23 +0100

    done

--- homework_26/js/main.js
+++ homework_26/js/main.js
@@ -1,23 +1,34 @@
 const container = document.querySelectorAll('ul')
 const liElement = document.querySelector('li')
 
-// const lastLi = container.lastElementChild
-// console.log(lastLi);
-
-// console.log(container);
-
-
+//додаємо клас last усім останнім li у групах
 for (i = 0; i < container.length; i++) {
     container[i].lastElementChild.classList.add('last')
 }
 
-
+//Ф-ція, яка встановлює першому li рівня - клас first
 function setFirstItemClassName(level) {
-    
+    const rootList = document.querySelector('.root');
+    const subList = document.querySelectorAll('.root ul');
+
+    switch (level) {
+        case 1: return rootList.firstElementChild.classList.add('first');
+        case 2: return subList.forEach(subUl => subUl.firstElementChild.classList.add('first'));
+        default : return 'неіснуючий рівень вкладеності'
+    }
 }
 
+setFirstItemClassName(2)
+// setFirstItemClassName(1)
 
 
+//перефарбовуємо класи в червоний (клас first) та зелений (клас last) з затримкою 2 секунди
+let lastLi = document.querySelectorAll('.last')
+setTimeout(() => lastLi.forEach(item => {item.style.backgroundColor = 'rgba(100, 255, 50, .5'}), 2000)
 
-// let lastLi = document.querySelectorAll('.last')
-// setTimeout(() => lastLi[i].style.backgroundColor = 'green', 2000)
+let firstLi = document.querySelectorAll('.first')
+setTimeout(() => firstLi.forEach(item => {item.style.backgroundColor = 'rgba(255, 10, 0, .5'}), 2000)
+
+
+//як альтернативу для setTimeout(), можна зробити затримку перефарбування через стилі:
+// lastLi.forEach(item => {item.style.backgroundColor = 'rgba(100, 255, 50, .5'; item.style.transition = 'background-color 2s'})
