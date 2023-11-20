422f92faa29c8ffad5a00f37c85a9bf16d90a087
Demianenko Nataliia <demiia.art@gmail.com>
2023-11-20 21:17:39 +0100

    done

--- homework_26/js/main.js
+++ homework_26/js/main.js
@@ -1,4 +1,4 @@
-const container = document.querySelectorAll('ul')
+/*const container = document.querySelectorAll('ul')
 const liElement = document.querySelector('li')
 
 //додаємо клас last усім останнім li у групах
@@ -32,3 +32,88 @@ setTimeout(() => firstLi.forEach(item => {item.style.backgroundColor = 'rgba(255
 
 //як альтернативу для setTimeout(), можна зробити затримку перефарбування через стилі:
 // lastLi.forEach(item => {item.style.backgroundColor = 'rgba(100, 255, 50, .5'; item.style.transition = 'background-color 2s'})
+*/
+
+/*//Start
+const root = document.querySelector('ul.root');
+console.log(root);
+
+const setFirstItemClassNameL1 = (level, ul) => {
+    const liFirst = ul.querySelector('li:first-of-type')
+    liFirst.classList.add('firstLevel')
+
+    //querySelectorAll
+    const allList = ul.querySelector('li')
+    console.log(allList);
+
+    //children
+    const list = ul.children;
+    console.log(list);
+
+    // list.forEach(li => console.log(li);) //error
+
+    for (let i = 0; i < list.length; i++) {
+        let li = list[i]
+    };
+
+
+}
+
+setFirstItemClassNameL1 (1, root);
+*/
+
+/*//!secondLevel
+
+const root2 = document.querySelector('ul.root');
+console.log(root2);
+
+const setFirstItemClassNameL2 = (level, ul) => {
+
+    // const liFirst = ul.querySelector('li:first-of-type')
+    // liFirst.classList.add('firstLevel')
+
+    const liOriginArray = [...ul.children]
+    console.log(liOriginArray);
+
+    liOriginArray.forEach(li => {
+        const innerUls = [...li.children]
+        console.log(innerUls);
+
+        innerUls.forEach(innerUl => innerUl.querySelector('li').classList.add('secondLevel'))
+    })
+}
+
+setFirstItemClassNameL2 (1, root2);
+*/
+
+//adding
+const root3 = document.querySelector('ul.root');
+console.log(root3);
+
+//доступ до всіх перших лішок будь-якої вкладеності
+// const setFirstItemClassNameL3WithQuerySelectorAll = (ul) => {
+//     const liFirst = ul.querySelectorAll('li:first-of-type')
+//     liFirst.forEach(li => li.classLIst.add('firstLevel'));
+// }
+
+const setFirstItemClassNameL3 = (level, ul) => {
+
+    level--
+
+    if ( level > 0 ) {
+        const liOriginArray = [...ul.children];
+    
+        liOriginArray.forEach(li => {
+    
+            const innerUls = [...li.children];
+            console.log(innerUls);
+    
+            innerUls.forEach(innerUl => setFirstItemClassNameL3(_, innerUl));
+        });
+    } else {
+        const liFirst = ul.querySelector('li:first-of-type');
+        liFirst.classList.add('firstLevel');
+    }
+};
+
+setFirstItemClassNameL3 (2, root3);
