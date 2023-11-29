bc7090fd7ce7127c8d55e02434cb9c79f3794813
Demianenko Nataliia <demiia.art@gmail.com>
2023-11-29 21:18:06 +0100

    done

--- homework_32/js/main.js
+++ homework_32/js/main.js
@@ -1,41 +1,52 @@
+
 const refs = {
     smileList: document.querySelector('#list'),
 }
 
-const liTag = refs.smileList.children
-
-const countField = document.createElement('p');
-
-
-//!!!!!!!!чому не може вставити динамічно????
-// refs.listLi.forEach(item => {
-//     item.appendChild(countField)
-// })
-
-let counter;
-
 function onSmileClick (event) {
-    // console.log(event.target);
-    // console.log(event.currentTarget);
 
     if (event.target.nodeName !== 'BUTTON') {
         return
     }
 
-    currentActiveBtn = event.currentTarget.querySelector('.tapButton');
+    const currentActiveBtn = event.currentTarget.querySelector('.tapButton');
+
+    const nexTapBtn = event.target;
+    nexTapBtn.classList.add('tapButton')
 
     if (currentActiveBtn) {
         currentActiveBtn.classList.remove('tapButton')
     }
 
-    const nexTapBtn = event.target;
-    nexTapBtn.classList.add('tapButton')
-    
-    const currentP = event.currentTarget.querySelector('.tapButton ~ .counter')
-    currentP.textContent = `${counter}`;
 
-    //як прив'язати індивідуальний лічильник для кожного елементу?
-    //або як задати counter всередині ф-ції?
+    //лічильник для кількості кліків
+    let currentCounter = nexTapBtn.parentNode.querySelector('.counter');
+
+    if (!currentCounter) {
+        currentCounter = document.createElement('p');
+        currentCounter.classList.add('counter');
+        nexTapBtn.parentNode.appendChild(currentCounter);
+    }
+
+    let currentValue = parseInt(currentCounter.textContent) || 0;
+    currentCounter.textContent = ++currentValue;
 }
 
+function tapColorAdd (event) {
+    if (event.target.nodeName !== 'BUTTON') {
+        return
+    }
+    event.target.style.boxShadow = 'inset 1px 2px 5px rgb(170, 170, 170), inset -1px -2px 5px rgb(170, 170, 170)';
+}
+
+function tapColorRemove (event) {
+    if (event.target.nodeName !== 'BUTTON') {
+        return
+    }
+    event.target.style.boxShadow = 'none'
+}
+
+
 refs.smileList.addEventListener('click', onSmileClick);
+refs.smileList.addEventListener('mousedown', tapColorAdd);
+refs.smileList.addEventListener('mouseup', tapColorRemove);
