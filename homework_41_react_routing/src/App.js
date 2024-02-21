ffe91cc8b5560d70d1a8a0d7c1c0de8fd442587e
Demianenko Nataliia <demiia.art@gmail.com>
2024-02-21 15:07:11 +0100

    start

---
+++ homework_41_react_routing/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/'>
+
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
